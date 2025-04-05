268342a1c7244c3a826e3c6b4523b62b48d1c6af
theocerdan <tcerdan.pro@gmail.com>
2025-04-05 22:19:00 +0200

    add new data

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,17 +3,36 @@ pragma solidity 0.8.28;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+    address: string,
+    name: string,
+    owner: string,
+    symbol: string,
+    url: string,
+    description: string,
+    supply: number
+    launchDate: number
+*/
+
 contract Token is ERC20 {
 
     address public immutable creator;
+    string public immutable image;
+    string public immutable description;
+    uint256 public immutable launchTimestamp;
 
     constructor(
         address _creator,
         string memory _name,
         string memory _symbol,
-        uint256 _totalSupply
+        uint256 _totalSupply,
+        string memory _image,
+        string memory _description
     ) ERC20(_name, _symbol) {
         creator = _creator;
+        image = _image;
+        description = _description;
+        launchTimestamp = block.timestamp;
 
         _mint(creator, _totalSupply);
     }
